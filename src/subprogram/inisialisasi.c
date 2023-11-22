81f122b0cda3d5e83b69114a3eb1a9dbe16ed4a3
RayNoor0 <rayendraalthaf04@gmail.com>
2023-11-22 12:26:50 +0700

    balasan

---
+++ src/subprogram/inisialisasi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void pesanpembuka(){
+    printf(".______    __    __  .______      .______    __  .______ \n");
+    printf("|   _  \\  |  |  |  | |   _  \\     |   _  \\  |  | |   _  \\\n");
+    printf("|  |_)  | |  |  |  | |  |_)  |    |  |_)  | |  | |  |_)  |\n");
+    printf("|   _  <  |  |  |  | |      /     |   _  <  |  | |      /\n");
+    printf("|  |_)  | |  `--'  | |  |\\  \\----.|  |_)  | |  | |  |\\ \\----.\n");
+    printf("|______/   \\______/  | _| `._____||______/  |__| | _| `._____|   \n");
+    printf("\n");
+    printf("Selamat datang di BurBir.\n");
+    printf("\n");
+    printf("Aplikasi untuk studi kualitatif mengenai perilaku manusia dengan menggunakan metode\n(pengambilan data berupa) Focused Group Discussion kedua di zamannya.\n");
+    printf("Silahkan masukan folder konfigurasi untuk dimuat: ");
+    //ambil config
+    printf("\n");
+    printf("File konfigurasi berhasil dimuat! Selamat berkicau!\n");
+}
